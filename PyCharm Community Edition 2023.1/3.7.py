4076d72ec77c3fd19f71e399a4ae8015736928b0
NenavizhyProekt <ostrouhov2013@gmail.com>
2023-11-29 10:22:54 +0300

    Add file

--- PyCharm Community Edition 2023.1/3.7.py
+++ PyCharm Community Edition 2023.1/3.7.py
@@ -1,8 +1,11 @@
 dictionary_1 = {'a': 300,
-                'b': 400}
+                'b': 400,
+                'e': 600}
 
 dictionary_2 = {'c': 500,
-                'd': 600}
+                'd': 600,
+                'f': 700,
+                'g': 800}
 
 dictionary_3 = dictionary_1 | dictionary_2
 
